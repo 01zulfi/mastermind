de202a119f9f263c5897ba2257dbf989c8fffec1
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-07-04 17:38:53 +0500

    Start work on mastermind - Add random_code method - Add CodeBreaker class - Add Game class -- Add start, calculate_hints and helper methods -- There's a bug in calculate_hints that needs fixing

---
+++ mastermind.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+
+require 'pry-byebug'
+
+def random_code
+  [rand(1..6), rand(1..6), rand(1..6), rand(1..6)].join.to_i
+end
+
+# CodeBreaker class
+class CodeBreaker
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+# Game loop
+class Game
+  def initialize
+    @player = CodeBreaker.new('human')
+    @code = random_code
+    @cracked = nil
+    @turn = 0
+  end
+
+  def start
+    code_breaker_move until game_end?
+    if @cracked
+      puts "#{@player.name} won."
+    else
+      puts 'Out of moves, computer won.'
+    end
+  end
+
+  private
+
+  def code_breaker_move
+    @turn += 1 
+    puts @code 
+    move = gets.chomp.to_i
+    if move == @code
+      @cracked = true
+    else
+      @cracked = false
+      puts calculate_hints(move)
+    end
+  end
+
+  def calculate_hints(move)
+    code_array = convert_integer_to_array(@code)
+    move_array = convert_integer_to_array(move)
+
+    hints = { correct: 0, close: 0 }
+
+    common_values = common_values_in_two_arrays(code_array, move_array)
+
+    common_values.each do |common_value|
+      indices_in_code = indices(code_array, common_value)
+      indices_in_move = indices(move_array, common_value)
+      if indices_in_code == indices_in_move
+        hints[:correct] += indices_in_code.length
+      else
+        common_indices = common_values_in_two_arrays(indices_in_code, indices_in_move)
+        # binding.pry
+        hints[:correct] += common_indices.length
+        hints[:close] += indices_in_move.length - common_indices.length
+        hints[:close] = 0 if hints[:close] < 0 
+      end
+    end
+    hints
+  end
+
+  def indices(array, element)
+    array.each_index.select { |index| array[index] == element }
+  end
+
+  def common_values_in_two_arrays(array_one, array_two)
+    array_one & array_two
+  end
+
+  def game_end?
+    @cracked || @turn >= 12
+  end
+
+  def convert_integer_to_array(integer)
+    integer.to_s.split('').map(&:to_i)
+  end
+end
+
+mastermind = Game.new
+mastermind.start
