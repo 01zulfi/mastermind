d9d8893b0ba1cbc5af42f6be0e617755110ac22b
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-07-06 16:23:11 +0500

    Update mastermind.rb - Add Display module - Add codebreaker_move method - Rename current Game class to HumanAsCodeBreaker - Add Game class - Remove unnecessary Player class - Move random_code method into HumanAsCodeBreaker class

--- mastermind.rb
+++ mastermind.rb
@@ -2,49 +2,73 @@
 
 require 'pry-byebug'
 
-def random_code
-  [rand(1..6), rand(1..6), rand(1..6), rand(1..6)].join
-end
+# for display methods
+module Display
+  def puts_player_won(player_name)
+    puts "#{player_name} won"
+  end
 
-# CodeBreaker class
-class CodeBreaker
-  attr_reader :name
+  def puts_player_out_of_moves
+    puts 'Out of moves, computer won.'
+  end
 
-  def initialize(name)
-    @name = name
+  def puts_turn(turn)
+    puts "Turn #{turn}"
   end
 end
 
-# Game loop
-class Game
+# Game to play when human is the codebreaker loop
+class HumanAsCodeBreaker
+  include Display
+
   def initialize
     @player = CodeBreaker.new('human')
     @code = random_code
     @cracked = nil
-    @turn = 0
+    @turn = 1
   end
 
   def start
     code_breaker_move until game_end?
     if @cracked
-      puts "#{@player.name} won."
+      puts_player_won(@player.name)
     else
-      puts 'Out of moves, computer won.'
+      puts_player_out_of_moves
     end
   end
 
   private
 
+  def random_code
+    [rand(1..6), rand(1..6), rand(1..6), rand(1..6)].join
+  end
+
   def code_breaker_move
+    puts_turn(@turn)
     @turn += 1
     puts @code
-    move = gets.chomp
+    move = codebreaker_move
     if move == @code
       @cracked = true
     else
       @cracked = false
       puts calculate_hints(move)
     end
+  end
+
+  def codebreaker_move
+    move = ''
+    first_move = true
+    until valid_move(move)
+      puts first_move ? 'Make your move!' : 'Invalid, make your move!'
+      move = gets.chomp
+      first_move = false
+    end
+    move
+  end
+
+  def valid_move(move)
+    move.length == 4 && move.scan(/\D/).empty?
   end
 
   def calculate_hints(move)
@@ -88,6 +112,37 @@ class Game
 
   def convert_string_to_array(string)
     string.split('').map(&:to_i)
+  end
+end
+
+# Game
+class Game
+  def initialize
+    @game_mode = game_mode_selection
+  end
+
+  def start
+    if @game_mode == '1'
+      HumanAsCodeBreaker.new.start
+    else
+      HumanAsCodeMaker.new.start
+    end
+  end
+
+  private
+
+  def game_mode_selection
+    puts 'Who do you want to play as?'
+    puts 'Enter 1 for Code Breaker'
+    puts 'Enter 2 for Code Maker'
+
+    selection = ''
+    selection = gets.chomp until valid_selection(selection)
+    selection
+  end
+
+  def valid_selection(selection)
+    %w[1 2].include?(selection)
   end
 end
 
