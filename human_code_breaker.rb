bd50ac75c6484ef63c11666714e0a378ad20602c
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-07-08 11:55:34 +0500

    Separate human codebreaker logic from mastermind - improve valid_code logic to only allow codes to be from 1 to 6

---
+++ human_code_breaker.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require './display'
+require './hints'
+
+# Game to play when human is the codebreaker loop
+class HumanCodeBreaker
+  include Display
+  include Hints
+
+  def initialize
+    @code_breaker = 'Human'
+    @code_maker = 'Computer'
+    @code = random_code
+    @cracked = nil
+    @turn = 1
+  end
+
+  def start
+    code_breaker_move until game_end?
+    if @cracked
+      put_player_won(@code_breaker)
+    else
+      puts_player_out_of_moves
+    end
+  end
+
+  private
+
+  def random_code
+    [rand(1..6), rand(1..6), rand(1..6), rand(1..6)].join
+  end
+
+  def code_breaker_move
+    puts_turn(@turn)
+    @turn += 1
+    puts @code
+    move = code_breaker_move_from_terminal
+    if move == @code
+      @cracked = true
+    else
+      @cracked = false
+      puts hints(@code, move)
+    end
+  end
+
+  def code_breaker_move_from_terminal
+    move = ''
+    first_move = true
+    until valid_move(move)
+      puts first_move ? 'Make your move!' : 'Invalid, make your move!'
+      move = gets.chomp
+      first_move = false
+    end
+    move
+  end
+
+  def valid_move(move)
+    correct_length = move.length == 4
+    only_integers = move.scan(/\D/).empty?
+    correct_integers = move.split('').map(&:to_i).all? { |integer| integer.between?(1, 6) }
+
+    correct_length && only_integers && correct_integers
+  end
+
+  def game_end?
+    @cracked || @turn >= 12
+  end
+end
--- mastermind.rb
+++ mastermind.rb
@@ -1,170 +1,8 @@
 # frozen_string_literal: true
 
-require 'pry-byebug'
-
-# for display methods
-module Display
-  def puts_player_won(player_name)
-    puts "#{player_name} won"
-  end
-
-  def puts_player_out_of_moves
-    puts 'Out of moves, computer won.'
-  end
-
-  def puts_turn(turn)
-    puts "Turn #{turn}"
-  end
-end
-
-# Game to play when human is the codebreaker loop
-class HumanAsCodeBreaker
-  include Display
-
-  def initialize
-    @player = 'Human'
-    @code = random_code
-    @cracked = nil
-    @turn = 1
-  end
-
-  def start
-    code_breaker_move until game_end?
-    if @cracked
-      puts_player_won(@player.name)
-    else
-      puts_player_out_of_moves
-    end
-  end
-
-  private
-
-  def random_code
-    [rand(1..6), rand(1..6), rand(1..6), rand(1..6)].join
-  end
-
-  def code_breaker_move
-    puts_turn(@turn)
-    @turn += 1
-    puts @code
-    move = codebreaker_move
-    if move == @code
-      @cracked = true
-    else
-      @cracked = false
-      puts calculate_hints(move)
-    end
-  end
-
-  def codebreaker_move
-    move = ''
-    first_move = true
-    until valid_move(move)
-      puts first_move ? 'Make your move!' : 'Invalid, make your move!'
-      move = gets.chomp
-      first_move = false
-    end
-    move
-  end
-
-  def valid_move(move)
-    move.length == 4 && move.scan(/\D/).empty?
-  end
-
-  def calculate_hints(move)
-    code_array = convert_string_to_array(@code)
-    move_array = convert_string_to_array(move)
-
-    hints = { correct: 0, close: 0 }
-
-    common_values = common_values_in_two_arrays(code_array, move_array)
-
-    common_values.each do |common_value|
-      indices_in_code = indices(code_array, common_value)
-      indices_in_move = indices(move_array, common_value)
-      calculate_hints_helper(hints, indices_in_code, indices_in_move)
-    end
-    hints
-  end
-
-  def calculate_hints_helper(hints, indices_in_code, indices_in_move)
-    if indices_in_code == indices_in_move
-      hints[:correct] += indices_in_code.length
-    else
-      common_indices = common_values_in_two_arrays(indices_in_code, indices_in_move)
-      hints[:correct] += common_indices.length
-      hints[:close] += [indices_in_code, indices_in_move].min_by(&:size).length - common_indices.length
-      hints[:close] = 0 if (hints[:close]).negative?
-    end
-  end
-
-  def indices(array, element)
-    array.each_index.select { |index| array[index] == element }
-  end
-
-  def common_values_in_two_arrays(array_one, array_two)
-    array_one & array_two
-  end
-
-  def game_end?
-    @cracked || @turn >= 12
-  end
-
-  def convert_string_to_array(string)
-    string.split('').map(&:to_i)
-  end
-end
-
-# Game to play when human is the codemaker
-class HumanAsCodeMaker
-  include Display
-
-  def initialize
-    @player = 'Computer'
-    @code = codemaker_move
-    @cracked = nil
-    @turn = 1
-  end
-
-  def start
-    code_breaker_move until game_end?
-    if @cracked
-      puts_player_won(@player)
-    else
-      puts_player_out_of_moves
-    end
-  end
-
-  private
-
-  def code_breaker_move
-    @turn += 1
-    if '0000'  == @code
-      @cracked = true
-    else
-      @cracked = false
-    end
-  end
-
-  def codemaker_move
-    move = ''
-    first_move = true
-    until valid_move(move)
-      puts first_move ? 'Create a code!' : 'Invalid, create a code!'
-      move = gets.chomp
-      first_move = false
-    end
-    move
-  end
-
-  def valid_move(move)
-    move.length == 4 && move.scan(/\D/).empty?
-  end
-
-  def game_end?
-    @cracked || @turn >= 12
-  end
-end
+require './display'
+require './human_code_breaker'
+require './computer_code_breaker'
 
 # Game
 class Game
@@ -174,9 +12,9 @@ class Game
 
   def start
     if @game_mode == '1'
-      HumanAsCodeBreaker.new.start
+      HumanCodeBreaker.new.start
     else
-      HumanAsCodeMaker.new.start
+      ComputerCodeBreaker.new.start
     end
   end
 
@@ -184,8 +22,8 @@ class Game
 
   def game_mode_selection
     puts 'Who do you want to play as?'
-    puts 'Enter 1 for Code Breaker'
-    puts 'Enter 2 for Code Maker'
+    puts 'Enter 1 to play as Code Breaker'
+    puts 'Enter 2 to play as Code Maker'
 
     selection = ''
     selection = gets.chomp until valid_selection(selection)
